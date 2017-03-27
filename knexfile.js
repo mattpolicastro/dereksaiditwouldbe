751a70ebea472c72cbee1550eba9be341d5bd226
Matt Policastro <matt@mattpolicastro.com>
2017-03-27 11:24:57 -0400

    Added knexfile and migrations for schema management
    Borked the database during a bad deployment through Ansible, so I re-did it with knex for safety.

---
+++ knexfile.js
@@ -0,0 +1,12 @@
+/*eslint linebreak-style: ["off", "unix"]*/
+'use strict';
+
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    connection: {
+      filename: 'src/daily.db'
+    }
+  }
+};
